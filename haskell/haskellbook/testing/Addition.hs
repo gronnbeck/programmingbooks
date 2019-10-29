0ac0f60e82fcc1c639d300dd51ec2a6a94f9e209
Ken Grønnbeck <kengroenn@gmail.com>
2019-10-29 06:24:02 +0100

    Haskell Programming Chapter 14.4

--- haskell/haskellbook/testing/Addition.hs
+++ haskell/haskellbook/testing/Addition.hs
@@ -1,6 +1,7 @@
 module Addition where
 
 import Test.Hspec
+import Test.QuickCheck
 
 main :: IO ()
 main = hspec $ do
@@ -17,6 +18,11 @@ main = hspec $ do
             multiply 2 2 `shouldBe` 4
         it "5 multipled by 3 is 15" $ do
             multiply 5 3 `shouldBe` 15
+        it "x + 1 is always greather than x" $ do
+            property $ prop_additionGreater
+
+runQc :: IO ()
+runQc = quickCheck prop_additionGreater
 
 dividedBy :: Integral a => a -> a -> (a, a)
 dividedBy num denom = go num denom 0
@@ -31,3 +37,26 @@ multiply a b = go a b 0
             | rest == 0 = res
             | otherwise =
                 go n (rest - 1) (res + n)
+
+prop_additionGreater :: Int -> Bool
+prop_additionGreater x = x + 1 > x
+
+genBool :: Gen Bool
+genBool = choose (False, True)
+
+genBool' :: Gen Bool
+genBool' = elements [False, True]
+
+genOrdering :: Gen Ordering
+genOrdering = elements [LT, EQ, GT]
+
+genChar :: Gen Char
+genChar = elements ['a'..'z']
+
+genTuple :: (Arbitrary a, Arbitrary b)
+         => Gen (a, b)
+genTuple = do
+    a <- arbitrary
+    b <- arbitrary
+    return (a, b)
+
