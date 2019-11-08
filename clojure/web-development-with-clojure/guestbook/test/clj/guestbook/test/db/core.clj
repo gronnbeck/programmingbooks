1680d65ddfbc868812559279bddfa8828a982f70
Ken Grønnbeck <kengroenn@gmail.com>
2019-11-08 08:34:54 +0100

    Work on Clojure Web Development Book

--- clojure/web-development-with-clojure/guestbook/test/clj/guestbook/test/db/core.clj
+++ clojure/web-development-with-clojure/guestbook/test/clj/guestbook/test/db/core.clj
@@ -19,19 +19,13 @@
 (deftest test-users
   (jdbc/with-db-transaction [t-conn *db*]
     (jdbc/db-set-rollback-only! t-conn)
-    (is (= 1 (db/create-user!
-               t-conn
-               {:id         "1"
-                :first_name "Sam"
-                :last_name  "Smith"
-                :email      "sam.smith@example.com"
-                :pass       "pass"})))
-    (is (= {:id         "1"
-            :first_name "Sam"
-            :last_name  "Smith"
-            :email      "sam.smith@example.com"
-            :pass       "pass"
-            :admin      nil
-            :last_login nil
-            :is_active  nil}
-           (db/get-user t-conn {:id "1"})))))
+    (is (= 1 (db/save-message!
+              t-conn
+              {:name "Bob"
+               :message "Hello, World"}
+              {:connection t-conn})))
+    (is (= {:name "Bob"
+            :message "Hello, World"}
+           (-> (db/get-messages t-conn {})
+               (first)
+               (select-keys [:name :message]))))))
