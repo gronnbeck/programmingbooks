e81459162212ab951c4fa6d85c910d4e81325a46
Ken Grønnbeck <kengroenn@gmail.com>
2020-08-25 20:15:07 +0200

    Clojure Web - to p. 22

---
+++ clojure/web-development-with-clojure/guestbook/src/clj/guestbook/routes/home.clj
@@ -0,0 +1,28 @@
+(ns guestbook.routes.home
+  (:require
+   [guestbook.layout :as layout]
+   [guestbook.db.core :as db]
+   [clojure.java.io :as io]
+   [guestbook.middleware :as middleware]
+   [ring.util.response]
+   [ring.util.http-response :as response]))
+
+(defn home-page [request]
+  (layout/render
+   request "home.html" {:messages (db/get-messages)}))
+
+(defn about-page [request]
+  (layout/render request "about.html"))
+
+(defn save-message! [{:keys [params]}]
+  (db/save-message! params)
+  (response/found "/"))
+
+(defn home-routes []
+  [""
+   {:middleware [middleware/wrap-csrf
+                 middleware/wrap-formats]}
+   ["/" {:get home-page}]
+   ["/about" {:get about-page}]
+   ["/message" {:post save-message!}]])
+
