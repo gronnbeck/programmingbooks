4f5ed4ca1ca143fd9bac0d2bf668ec0aa195df96
Ken Grønnbeck <kengronnbeck@Kens-MacBook-Pro.local>
2020-09-08 18:51:42 +0200

    Clojure Web - p. 44
    Complete the chapter on Ring and Reitit.
    
    Continue with HTML Templating Using Selmer

--- clojure/web-development-with-clojure/ring-app/src/ring_app/core.clj
+++ clojure/web-development-with-clojure/ring-app/src/ring_app/core.clj
@@ -1,7 +1,19 @@
 (ns ring-app.core
-  (:require [ring.adapter.jetty :as jetty]
-            [ring.util.response :as response]
+  (:require [reitit.ring :as reitit]
+            [ring.adapter.jetty :as jetty]
+            [muuntaja.middleware :as muuntaja]
+            [ring.util.http-response :as response]
             [ring.middleware.reload :refer [wrap-reload]]))
+
+(defn http-handler [request-map]
+  (response/ok
+   (str "<html><body>Hello, your IP is: "
+              (:remote-addr request-map)
+              "</body></html>")))
+
+(defn json-handler [request]
+  (response/ok
+   {:result (get-in request [:body-params :id])}))
 
 (defn wrap-nocache [handler]
   (fn [request]
@@ -9,11 +21,36 @@
         handler
         (assoc-in [:headers "Pragma"] "no-cache"))))
 
-(defn handler [request-map]
-  (response/response
-   (str "<html><body>Hello, your IP is: "
-              (:remote-addr request-map)
-              "</body></html>")))
+(defn wrap-formats [handler]
+  (-> handler
+      (muuntaja/wrap-format)))
+
+(def routes
+  [["/" {:get http-handler
+         :post http-handler}]
+   ["/echo/:id"
+    (fn [{{:keys [id]} :path-params}]
+      (response/ok (str "<p>the value is: " id "</p>")))]
+   ["/api"
+    {:middleware [wrap-formats]}
+     ["/multiply"
+      {:post
+       (fn [{{:keys [a b]} :body-params}]
+         (response/ok {:result (* a b)}))}]]])
+
+(def handler
+  (reitit/routes
+   (reitit/ring-handler
+    (reitit/router routes))
+   (reitit/create-resource-handler
+    {:path "/"})
+   (reitit/create-default-handler
+    {:not-found
+     (constantly (response/not-found "404 - Page not found"))
+     :method-not-allowed
+     (constantly (response/method-not-allowed "405 - Not allowed"))
+     :not-accetable
+     (constantly (response/not-acceptable "406 - Not acceptable"))})))
 
 (defn -main []
   (jetty/run-jetty
