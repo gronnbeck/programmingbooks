b073ae623e64a9c2db55dccfc0d55732f259a25f
Ken Grønnbeck <ken.gronnbeck@kolonial.no>
2020-09-13 19:10:56 +0200

    Skipped rest of the chapter about HTML templating

---
+++ clojure/web-development-with-clojure/html-templating/src/html_templating/core.clj
@@ -0,0 +1,10 @@
+(ns html-templating.core
+  (:require [selmer.parser :as selmer]))
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+
+(selmer/render "Hello, {{name}}" {:name "World"})
